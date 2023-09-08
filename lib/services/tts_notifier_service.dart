2e3895c56e42e16ecf3a62ac81e88128e27316aa
isem <ise97kk@gmail.com>
2023-09-08 18:16:19 +0900

    振動機能追加

--- lib/services/tts_notifier_service.dart
+++ lib/services/tts_notifier_service.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_tts/flutter_tts.dart';
+import 'package:vibration/vibration.dart';
 
 class TTSNotifier {
   final FlutterTts flutterTts = FlutterTts();
@@ -24,13 +25,19 @@ class TTSNotifier {
     // TODO: ある大きさ以上の物体(車、人)が検出されたときは「危険です、避けてください」的な内容に分岐させる
     var objSize = object[1];
     String message;
+    var duration;
     if (objSize.width > 500 && objSize.height > 300) {
       message = "危険です。${object[0]}が$directionの方向にあります。避けて下さい。";
+      duration = 5000;
     } else if (objSize.width >= 300 && objSize.height >= 150) {
       message = "${object[0]}が$directionの方向にあります。気を付けて下さい。";
+      duration = 3000;
     } else {
       message = "${object[0]}が$directionの方向にあります。";
+      duration = 1000;
     }
+    Vibration.vibrate(duration: duration);
+
     speak(message);
   }
 }
