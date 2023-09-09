982c631438b5200099cd7acdff2421dfa6f26e3a
isem <ise97kk@gmail.com>
2023-09-09 15:49:43 +0900

    犬の音声出力遅延修正　その２

--- lib/services/audio_service.dart
+++ lib/services/audio_service.dart
@@ -3,13 +3,19 @@ import 'package:audioplayers/audioplayers.dart';
 class AudioService {
   final player = AudioCache();
   final AudioPlayer audioPlayer = AudioPlayer();
+  bool isCurrentlyPlaying = false;
 
   Future<void> playSound(num type) async {
+    if (isCurrentlyPlaying) {
+      return;
+    }
+    isCurrentlyPlaying = true;
     if (type == 0) {
       await player.play('audios/dog1b.mp3');
     } else if (type == 1) {
       await player.play('audios/dog2times.mp3');
     }
     await audioPlayer.onPlayerCompletion.first;
+    isCurrentlyPlaying = false;
   }
 }
