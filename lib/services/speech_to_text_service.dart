b2054110ac1a9f598091dc1b98d7c91cbfda2d43
Naoya-Yasuda <2301330039zz@cyber-u.ac.jp>
2023-09-12 11:15:28 +0900

    音声認識復活

--- lib/services/speech_to_text_service.dart
+++ lib/services/speech_to_text_service.dart
@@ -1,6 +1,7 @@
 import 'package:speech_to_text/speech_to_text.dart' as stt;
 import 'package:speech_to_text/speech_recognition_result.dart';
 import 'tts_notifier_service.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class SpeechToTextService {
   final stt.SpeechToText _speech = stt.SpeechToText();
@@ -13,9 +14,15 @@ class SpeechToTextService {
   late final TTSNotifier tTSNotifier = TTSNotifier(); // lateを使う
 
   Future<void> initialize() async {
+    bool hasPermission = await _requestMicrophonePermission();
+    if (!hasPermission) {
+      print("Microphone permission was not granted");
+      return; // Return early if permission is not granted
+    }
     bool available = await _speech.initialize(
       onStatus: _onStatus,
       onError: _onError,
+      debugLogging: true,
     );
     if (!available) {
       // Handle the error appropriately
@@ -33,6 +40,16 @@ class SpeechToTextService {
     } catch (e) {
       print('--------- SpeechToTextService.startListening3:' + e.toString());
     }
+  }
+
+  Future<bool> _requestMicrophonePermission() async {
+    PermissionStatus permissionStatus = await Permission.microphone.status;
+
+    if (permissionStatus.isDenied || permissionStatus.isRestricted) {
+      permissionStatus = await Permission.microphone.request();
+    }
+
+    return permissionStatus.isGranted;
   }
 
   Future<void> stopListening() async {
@@ -78,6 +95,7 @@ class SpeechToTextService {
 
   void _onStatus(String status) {
     _isListening = status == stt.SpeechToText.listeningStatus;
+    print('_onStatus: $_isListening');
   }
 
   void _onError(dynamic error) {
